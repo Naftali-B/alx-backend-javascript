2c55e9bfddbf38dc4c96e604644da08f5aaba91a
Naftali-B <nbudamba@gmail.com>
2024-04-30 16:32:10 +0300

    get_list

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
+
 const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
-  if (position > int8Array.length) throw new Error('Position outside range');
+  if (position >= int8Array.length) throw new Error('Position outside range');
   int8Array[position] = value;
   return new DataView(buffer);
 };
